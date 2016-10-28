5978c965751f4036555cbfb87ba72b231f804661
DerekYu <derek-nis@hotmail.com>
2016-10-28 12:27:29 -0400

    [Derek] p13euler solutions

--- p13euler.rb
+++ p13euler.rb
@@ -1,11 +1,12 @@
 class Problem
   def initialize
-    path_to_file = "~/Documents/workspace/testbed/p13euler_data.txt"
-    sum_of_10_digits = sum_10(path_to_file)
+    file_name = "/p13euler_data.txt"
+    sum_of_10_digits = sum_10(file_name)
     p (sum_of_10_digits.to_s)[0...10]
   end
 
-  def sum_10(path_to_file)
+  def sum_10(file_name)
+    path_to_file = __dir__ + file_name
     File.open(path_to_file, "r") do |f|
       sum = 0
       f.each_line do |line|
